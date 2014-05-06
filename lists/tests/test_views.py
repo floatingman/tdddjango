0b65af6f1fd043457fb9f3ef7b2c59033d2802b5
Daniel Newman <dwnewman78@gmail.com>
2014-05-06 16:15:12 -0500

    added validation for duplicate items

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils.html import escape
 from unittest import skip
 
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import (DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR, ExisitingListItemForm, ItemForm,)
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
@@ -69,7 +69,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExisitingListItemForm)
         self.assertContains(response, 'name="text"')
 
     def test_displays_only_items_for_that_list(self):
@@ -126,19 +126,18 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExisitingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_LIST_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
         response = self.client.post('/lists/%d/' % (list1.id,), data={'text': 'textey'})
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
